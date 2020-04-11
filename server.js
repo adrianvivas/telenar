30715465132af6b52579d73aec5ca7c68cd3b1f4
Adrian Vivas <medcannapo@gmail.com>
2020-04-11 18:46:40 -0500

    App name changed

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/talenar'));
+app.use(express.static(__dirname + '/dist/telenar'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/talenar/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/telenar/index.html'));
 });
 
 // default Heroku PORT
