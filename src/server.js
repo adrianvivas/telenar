1c81cc69f26190f8be9cbef8827dd7a9d19f4217
Adrian Vivas <medcannapo@gmail.com>
2020-04-10 21:08:20 -0500

    third deploy with heroku

--- src/server.js
+++ src/server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/telenar'));
+app.use(express.static(__dirname + '/dist/talenar'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/telenar/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/talenar/index.html'));
 });
 
 // default Heroku PORT
