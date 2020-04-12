dab0779208b39b981d6e3949160c759205952286
Adrian Vivas <medcannapo@gmail.com>
2020-04-11 19:02:15 -0500

    all talenars changed to telenar

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('talenar app is running!');
+    expect(page.getTitleText()).toEqual('telenar app is running!');
   });
 
   afterEach(async () => {
